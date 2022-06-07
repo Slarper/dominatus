62ba08e1d9d043e69393a14752034bf4527492b5
Slarper <naivesimple@outlook.com>
2022-06-08 01:38:57 +0800

    Logger Commit

---
+++ src/main/java/slarper/dominatus/Dominatus.java
@@ -0,0 +1,11 @@
+package slarper.dominatus;
+
+public class Dominatus {
+    public static void main(String[] args){
+        Reference.LOGGER.trace("TRACE");
+        Reference.LOGGER.debug("DEBUG");
+        Reference.LOGGER.info("INFO");
+        Reference.LOGGER.warn("WARN");
+        Reference.LOGGER.error("Error");
+    }
+}
---
+++ src/main/java/slarper/dominatus/Reference.java
@@ -0,0 +1,9 @@
+package slarper.dominatus;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Reference {
+    public static final String DOMINATUS_ID = "dominatus";
+    public static final Logger LOGGER = LoggerFactory.getLogger(DOMINATUS_ID);
+}
